2fb2e23ff0e100ade1bac9323ca6663a9e858e3e
isaquearaujo97 <1115243@senacminas.edu.br>
2026-06-30 15:02:59 -0300

    v1.6

--- java/calcukaylany.js
+++ java/calcukaylany.js
@@ -5,6 +5,11 @@ const n2 = parseFloat(document.getElementById('nota2').value);
 const n3 = parseFloat(document.getElementById('nota3').value);
 const n4 = parseFloat(document.getElementById('nota4').value);
 
+if (n1 , n2 , n3 , n4 > 25) {
+        alert("teste");
+        return;
+    }
+
 const divResultado = document.getElementById('resultado');
 
     if (isNaN(n1) || isNaN(n2) || isNaN(n3) || isNaN(n4)) {
@@ -13,18 +18,28 @@ const divResultado = document.getElementById('resultado');
     divResultado.textContent = " Por favor, digite as 4 notas.";
     return;
 }
-const mediaFinal = (n1 + n2 + n3 + n4) / 4;
+const mediaFinal = (n1 + n2 + n3 + n4);
 
-const mediaAprovacao = 6.0;
+if (n1 > 25 || n2 > 25 || n3 >25 || n4 > 25) {
+        alert("teste");
+        return;
+    }
+
+
+const mediaAprovacao = 75;
 
 divResultado.style.display = "block";
 
 
 if (mediaFinal >= mediaAprovacao) {
-divResultado.className = "aprovado";
-divResultado.innerHTML = ` Aprovado!<br>Sua média foi: ${mediaFinal.toFixed(1)}`;
+    if (mediaFinal < 90) {
+        divResultado.className = "aprovado";
+        divResultado.innerHTML = ` Aprovado!<br>Sua média foi: ${mediaFinal.toFixed(1)}`}
+    else  {
+        divResultado.className = "aprovado";
+        divResultado.innerHTML = ` Aprovado sua nota foi exelente!<br>Sua média foi: ${mediaFinal.toFixed(1)}`;} 
 } else {
 divResultado.className = "reprovado";
-divResultado.innerHTML = ` Recuperação!<br>Sua média foi: ${mediaFinal.toFixed(1)}`;
+divResultado.innerHTML = `Você terá que fazer recuperação <br>Sua média foi: ${mediaFinal.toFixed(1)}`;
 }
 }
